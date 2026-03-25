b4ec82547ce7669fd9ae9bd63cf30bc0614cc73d
Torben Ewert <Torben@eWert-Online.com>
2026-03-25 19:34:04 +0100

    pinc 0.0.2

--- Formula/pinc.rb
+++ Formula/pinc.rb
@@ -3,6 +3,9 @@ class Pinc < Formula
   homepage "https://github.com/pinc-official/pinc-playground"
   license "MIT"
 
+  url "https://github.com/pinc-official/pinc-playground/archive/refs/tags/0.0.2.tar.gz"
+  sha256 "a4f8aa7f8f9332ba23e7d2a68d7f23af9e13fcf935475f110bf903bad295558f"
+
   bottle do
     root_url "https://github.com/pinc-official/homebrew-tap/releases/download/0.0.2"
     sha256 cellar: :any_skip_relocation, arm64_sequoia: "e77861fd14ab39f995d8d274bcb60166d7fec73ddbed522415b6e5471df29120"
